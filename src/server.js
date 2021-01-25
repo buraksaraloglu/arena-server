e4f03935efbfed58231a8ad737ff6a57005f25a0
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-25 16:01:27 +0300

    cors bug fix try

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@
 /* eslint-disable no-param-reassign */
 const corsOptions = {
   cors: {
-    origin: 'https://arena-bs.netlify.app/',
+    origin: ['https://arena-bs.netlify.app/', '*'],
+    methods: ['GET', 'POST'],
   },
 };
 const app = require('express')();
